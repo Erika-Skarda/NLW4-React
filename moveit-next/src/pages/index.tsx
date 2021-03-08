431b481f75b0ccd5b3ed0631d79a4a08e7048273
Erika-Skarda <erikaskarda@yahoo.com.br>
2021-03-08 13:27:21 -0300

    Class 3

---
+++ moveit-next/src/pages/index.tsx
@@ -0,0 +1,31 @@
+//npm i typescript @types/react @types/react-dom @types/node -D
+//Head é como o head do html
+import Head from 'next/head';
+import React from 'react';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        
+        <title>Início | move.it</title>
+      </Head>
+     <ExperienceBar />
+
+     <section>
+       <div >
+         <Profile />
+         <CompletedChallenges/>
+         <Countdown />
+       </div>
+         <ChallengeBox />
+     </section>
+    </div>
+  )
+}
